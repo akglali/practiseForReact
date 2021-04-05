9dd815011dd2122d4e737ab14511819968201546
U370709069 <akgol97_@hotmail.com>
2021-04-05 22:50:28 +0300

    learned how to use component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { Component} from "react";
 import './custom.css';
 import Card from './Card';
 import {ThemeProvider} from 'styled-components';
@@ -11,77 +11,98 @@ const theme = {
 }
 
 
+
 // import faker from 'faker';
 
-function App() {
-    const [showCard, setShowCard] = useState(true);
+class App extends Component {
 
-    const [cards, setCards] = useState([
-            {
-                key: '1',
-                name: 'Ali Akgol',
-                title: 'Computer Engineering',
-                image: 'https://cdn.fakercloud.com/avatars/divya_128.jpg'
-            },
-            {
-                key: '2',
-                name: 'Sultan Guvenbas',
-                title: 'Computer Scientist',
-                image: 'https://cdn.fakercloud.com/avatars/divya_128.jpg'
-            },
-            {
-                key: '3',
-                name: 'Ahmet Dogan',
-                title: 'Computer Engineering',
-                image: 'https://cdn.fakercloud.com/avatars/divya_128.jpg'
-            }
-        ]
-    )
-    const deleteCard = (cardIndex) => {
-        const cards_copy = [...cards]
-        cards_copy.splice(cardIndex, 1)
-        setCards(cards_copy)
+    constructor(props) {
+        super(props);
+        this.state={
+            cards:[
+                {
+                    key: '1',
+                    name: 'Ali  Akgol',
+                    title: 'Computer Engineering',
+                    image: 'https://cdn.fakercloud.com/avatars/divya_128.jpg'
+                },
+                {
+                    key: '2',
+                    name: 'Sultan Guvenbas',
+                    title: 'Computer Scientist',
+                    image: 'https://cdn.fakercloud.com/avatars/divya_128.jpg'
+                },
+                {
+                    key: '3',
+                    name: 'Ahmet Dogan',
+                    title: 'Computer Engineering',
+                    image: 'https://cdn.fakercloud.com/avatars/divya_128.jpg'
+                }
+            ],
+            showCard:true
+        }
 
     }
-    const changeName = (event, key) => {
-        //1. Which card is deleted
-        const cardIndex = cards.findIndex(card => card.key === key);
 
+
+
+
+
+        toggleShowCard = () => this.setState({showCard: !this.state.showCard})
+
+        deleteCard = (cardIndex) => {
+        const cards_copy = [...this.state.cards]
+        cards_copy.splice(cardIndex, 1)
+        this.setState({cards:cards_copy})
+
+    }
+        changeName = (event, key) => {
+        //1. Which card is deleted
+        const cardIndex = this.state.cards.findIndex(card => card.key === key);
         //2. make the card list copy
-        const cards_copy = [...cards];
+        const cards_copy = [...this.state.cards];
         //3. change the specific card
         cards_copy[cardIndex].name = event.target.value;
         //4. set the last version
-        setCards(cards_copy)
+            this.setState({cards:cards_copy})
+
+        }
+
+
+
+    render() {
+
+
+        const classes = ['button'];
+        if (this.state.cards.length < 3) classes.push('pink');
+        if (this.state.cards.length < 2) classes.push('red text');
+
+        const hideAndShowCard = this.state.showCard && (
+            this.state.cards.map((card, index) => <Card image={card.image}
+                                             name={card.name}
+                                             department={card.title}
+                                             key={card.key}
+                                             onDelete={() => this.deleteCard(index)}
+                                             onChangeName={(event) => this.changeName(event, card.key)}
+            >
+
+
+            </Card>));
+        return (
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <Button color='mango' length={this.state.cards.length} onClick={this.toggleShowCard}>Toogle</Button>
+                    <button className={classes.join(' ')} onClick={this.toggleShowCard}>Toggle Show and Height</button>
+                    {hideAndShowCard}
+
+                </div>
+            </ThemeProvider>
+        );
     }
-    const classes = ['button'];
-    if (cards.length < 3) classes.push('pink');
-    if (cards.length < 2) classes.push('red text');
-
-    const toggleShowCard = () => setShowCard(!showCard);
-
-    const hideAndShowCard = showCard && (
-        cards.map((card, index) => <Card image={card.image}
-                                         name={card.name}
-                                         department={card.title}
-                                         key={card.key}
-                                         onDelete={() => deleteCard(index)}
-                                         onChangeName={(event) => changeName(event, card.key)}
-        >
 
 
-        </Card>));
 
-    return (
-        <ThemeProvider theme={theme}>
-            <div className="App">
-                <Button color='mango' length={cards.length}>Toogle</Button>
-                <button className={classes.join(' ')} onClick={toggleShowCard}>Toggle Show and Height</button>
-                {hideAndShowCard}
 
-            </div>
-        </ThemeProvider>
-    );
 }
 
 export default App;
